9228245d70f7e5567be28238c0ffb1b053a40865
https://github.com/BelenAlegrette <108761268+BelenAlegrette@users.noreply.github.com>
2023-12-13 03:51:51 -0300

    Agrega en settings, Database

--- salon_project/settings.py
+++ salon_project/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'salon_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BelAlegrette$salon_project',
+        'USER' : 'BelAlegrette',
+        'PASSWORD': 'Cirito11',
+        'HOST': 'BelAlegrette.mysql.pythonanywhere-services.com',
+        'PORT': '3306'
     }
 }
 
