6364fe534660803da1aff64445272d05a54f36fc
https://github.com/BelenAlegrette <108761268+BelenAlegrette@users.noreply.github.com>
2023-12-14 11:56:35 -0300

    Cambios en settings.py

--- salon_project/settings.py
+++ salon_project/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'salon_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
- DATABASES = {
+"""  DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'BelAlegrette$salon_project',
@@ -92,14 +92,14 @@ WSGI_APPLICATION = 'salon_project.wsgi.application'
         'HOST': 'BelAlegrette.mysql.pythonanywhere-services.com',
         'PORT': '3306'
     }
-} 
+}  """
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
+} 
 
 
 # Password validation
