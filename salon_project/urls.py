e7283d7fefd85da8295f37f86f0b6e6c5c083729
https://github.com/BelenAlegrette <108761268+BelenAlegrette@users.noreply.github.com>
2023-12-14 14:44:47 -0300

    Ajustes

--- salon_project/urls.py
+++ salon_project/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.urls import include
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import Index
 
 urlpatterns = [
@@ -26,3 +29,5 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('evento/', include('salon_app.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
